f35c8a2fe740e39f31aa94008cc9fd03bfa2a0e3
carobte <caro.bustamante.escobar@gmail.com>
2024-04-16 10:36:18 -0500

    opciones para declarar strings

---
+++ src/js/main.js
@@ -0,0 +1,9 @@
+// #Declaración de strings
+
+const option1 = "Hello world"
+const option2 = String("Hello world")
+const option3 = new String("Hello world")
+
+console.log(option1)
+console.log(option2)
+console.log(option3) // String {'Hello world'}
